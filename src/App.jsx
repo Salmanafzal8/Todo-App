04c4d7bb6dc1deb7f6e0d0a1b83329e97327a27b
Salman Afzal <salmanafzalozil8@gmail.com>
2025-07-21 22:15:06 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <input type="text" name="" id="" />
+      <button>SUBMIT</button>
+      
+    </div>
+  )
+}
+export default App
